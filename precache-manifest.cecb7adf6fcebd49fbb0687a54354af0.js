b9f91341f5ccdcb75f4fe5c89612855278968a30
Kenneth Wilcox <kenneth.wilcox@purered.net>
2019-10-16 10:43:22 -0400

    Updates

---
+++ precache-manifest.cecb7adf6fcebd49fbb0687a54354af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fc4f38e77e9feda0fd6be1f33dcf60",
+    "url": "/react_training/index.html"
+  },
+  {
+    "revision": "a7cc5d8cc599df8a0ecb",
+    "url": "/react_training/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "aa66b07eee1dc307e449",
+    "url": "/react_training/static/css/main.50a46eee.chunk.css"
+  },
+  {
+    "revision": "a7cc5d8cc599df8a0ecb",
+    "url": "/react_training/static/js/2.1b113841.chunk.js"
+  },
+  {
+    "revision": "aa66b07eee1dc307e449",
+    "url": "/react_training/static/js/main.6b7d5dab.chunk.js"
+  },
+  {
+    "revision": "e3bc6fe89f3cc6c355c2",
+    "url": "/react_training/static/js/runtime-main.28c91934.js"
+  }
+]);
